e773a53ae829b1c13dc54fc77f101cb9a505103e
ryot0720 <ryo.tanaka0720@gmail.com>
2017-06-06 20:33:09 +0900

    user_controllerの変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
-
-    if @user.update(user_params)
+    if current_user.update(user_params)
       redirect_to :root
     else
       redirect_to action: :edit
