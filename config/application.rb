d29ee1bb7d047f823747850b04e566661d8d35aa
ryot0720 <ryo.tanaka0720@gmail.com>
2017-06-07 16:07:16 +0900

    フラッシュメッセージの日本語化

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,9 @@ module ChatSpace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    
+
+    config.i18n.default_locale = :ja
+
     config.generators do |g|
         g.javascripts false
         g.stylesheets false
