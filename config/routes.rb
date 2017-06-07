343a66c7520c0a4946d3d898c56f531f7a54cc89
ryot0720 <ryo.tanaka0720@gmail.com>
2017-06-07 18:02:57 +0900

    Group機能のルーティング及びメッセージ機能とのネスト構築

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
   resources :users, only: [:edit, :update]
-  resources :messages, only: :index
+  resources :groups, only: [:new, :create, :edit, :update, :index] do
+    resources :messages, only: :index
+  end
 end
