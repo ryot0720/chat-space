12200846cd2b7ebff4b351dfdcc318a022c1172f
ryot0720 <ryo.tanaka0720@gmail.com>
2017-06-08 16:52:15 +0900

    グループ作成用コントローラの設定

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,14 +3,28 @@ class GroupsController < ApplicationController
   end
 
   def new
+    @group = Group.new
   end
 
   def create
+    @group = Group.new(group_params)
+      if @group.save
+         flash[:notice] = "グループを作成しました"
+         redirect_to root_path
+      else
+        session[:errors] = @group.errors.full_messages
+        render :new
+      end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, { user_ids:[] })
   end
 end
