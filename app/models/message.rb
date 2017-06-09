222795b695c8db5f26639149c7635e7708f685dc
ryot0720 <ryo.tanaka0720@gmail.com>
2017-06-09 13:28:57 +0900

    Messageモデルのavatarとアップローダーの紐付け・バリデーションの追加

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
+
+  mount_uploader :avatar, ImageUploader
+  validates_presence_of :text||validates_presence_of :avatar
 end
