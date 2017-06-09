4ab45c22e823aed52e2d0a82b58cb1d097741f09
ryot0720 <ryo.tanaka0720@gmail.com>
2017-06-09 09:20:30 +0900

    グルーオプ作成及び編集のためのコントローラーの設定

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
   def index
+    @groups = Group.all
   end
 
   def new
@@ -18,9 +19,23 @@ class GroupsController < ApplicationController
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
   def update
+    @group = Group.find(params[:id])
+    if @group.save(group_params)
+      flash[:notice] = "チャットグループが更新されました"
+      redirect_to root_path
+    else
+      session[:errors] = @group.errors.full_messages
+      render :edit
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, { user_ids:[] })
   end
 
   private
