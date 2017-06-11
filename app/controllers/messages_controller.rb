498e0c53667f39ae5d88a217e99a02e64eecbca7
ryot0720 <ryo.tanaka0720@gmail.com>
2017-06-11 19:16:21 +0900

    メッセージ送信機能のためのコントローラー設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,29 @@
 class MessagesController < ApplicationController
+  before_action :get_group
+
   def new
+    @message = Message.new
   end
 
   def create
+    @message = @group.messages.new(message_params)
+    if @message.save
+      flash[:notice] = "メッセージが送信されました"
+      redirect_to new_group_message_path
+    else
+      flash[:alert] = " メッセージを入力してください"
+      render :new
+    end
   end
 
   private
-  def avatar_params
-    params.require(:message).permit(:text, :image)
+  def get_group
+    @messages = Group.find(params[:group_id]).messages
+    @groups = current_user.groups.includes(:messages)
+    @group = Group.find(params[:group_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:text, :avatar)
   end
 end
