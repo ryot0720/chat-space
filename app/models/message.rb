56822140a3028428b76432e80d4f0aa63b4ae6c9
ryot0720 <ryo.tanaka0720@gmail.com>
2017-06-12 15:09:21 +0900

    どちらかのカラムには値がなければならないというvalidationに修正

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,6 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
   mount_uploader :avatar, AvatarUploader
-  validates :text, presence: true
-  validates :avatar, presence: true
+  validates :text, presence: true, if: -> { avatar.blank? }
+  validates :avatar, presence: true, if: -> { text.blank? }
 end
