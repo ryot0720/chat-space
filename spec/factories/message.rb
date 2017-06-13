425f09df97acfcc682952361ba7b14fbcea2e5ee
ryot0720 <ryo.tanaka0720@gmail.com>
2017-06-13 15:12:12 +0900

    テキストのダミー化

--- spec/factories/message.rb
+++ spec/factories/message.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
 
   factory :message do
-    text   "テキストテキスト"
-    avatar  { fixture_file_upload("spec/fixtures/スクリーンショット 2017-06-07 11.53.11.png", "avatar/jpeg") }
+    text     { Faker::Lorem.paragraph}
+    avatar   { fixture_file_upload("spec/fixtures/スクリーンショット 2017-06-07 11.53.11.png", "avatar/jpeg") }
     group_id 40
-    user_id 15
+    user_id  15
   end
 
 end
