8177f340d382c6019df86b9a6b613bd9f04f8f69
ryot0720 <ryo.tanaka0720@gmail.com>
2017-06-13 14:31:09 +0900

    validationの追加

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,6 @@ class Message < ApplicationRecord
   mount_uploader :avatar, AvatarUploader
   validates :text, presence: true, if: -> { avatar.blank? }
   validates :avatar, presence: true, if: -> { text.blank? }
+  validates :group_id, presence: true
+  validates :user_id, presence: true
 end
